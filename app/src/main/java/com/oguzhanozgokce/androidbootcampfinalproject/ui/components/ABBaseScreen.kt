cfb9edb3bb0158f04f3520e2b3a6870bb49062a5
oguzhanozgokce <ozgokceoguzhan34@gmail.com>
2025-07-24 20:04:41 +0300

    rename KXBaseScreen to ABBaseScreen and remove unused import

--- app/src/main/java/com/oguzhanozgokce/androidbootcampfinalproject/ui/components/ABBaseScreen.kt
+++ app/src/main/java/com/oguzhanozgokce/androidbootcampfinalproject/ui/components/ABBaseScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.oguzhanozgokce.androidbootcampfinalproject.common.collectWithLifecycle
